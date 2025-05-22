05eebefccd8e479244cdff3ef8085b6975e2d3fb
MuhamedSuhail <s.j.muhamedsuhail@outlook.in>
2025-05-22 09:01:57 +0530

    first commit

--- frontend/src/components/HoldingsTable/index.jsx
+++ frontend/src/components/HoldingsTable/index.jsx
@@ -3,13 +3,18 @@ import "./index.css";
 
 const Holdings = ({ CoinsList }) => {
   const [selected, setSelected] = useState({});
+  const [allView, setAllView] = useState(true);
 
-  const data = CoinsList;
-
-  const toggleSelection = (symbol) => {
-    setSelected((prev) => ({ ...prev, [symbol]: !prev[symbol] }));
+  const toggleSelection = (coin) => {
+    setSelected((prev) => ({ ...prev, [coin]: !prev[coin] }));
   };
 
+  console.log("selected", selected);
+
+  const changeView = () => {
+    setAllView((prev) => !prev);
+  };
+  const updatedViewlist = allView ? CoinsList.slice(0, 4) : CoinsList;
   return (
     <div className="holdings">
       <h2>Holdings</h2>
@@ -18,7 +23,11 @@ const Holdings = ({ CoinsList }) => {
           <tr className="table-header">
             <th>Select</th>
             <th>Asset</th>
-            <th>Holdings<br/><span>Avg Price</span></th>
+            <th>
+              Holdings
+              <br />
+              <span>Avg Price</span>
+            </th>
             <th>Current Price</th>
             <th>Short-Term</th>
             <th>Long-Term</th>
@@ -26,15 +35,12 @@ const Holdings = ({ CoinsList }) => {
           </tr>
         </thead>
         <tbody>
-          {data.map((asset, idx) => (
-            <tr
-              key={idx}
-              className={selected ? "selected-row" : ""}
-            >
+          {updatedViewlist.map((asset, idx) => (
+            <tr key={idx} className={selected[asset.coinName] ? "selected-row" : ""}>
               <td>
                 <input
                   type="checkbox"
-                  onChange={() => toggleSelection(asset.symbol)}
+                  onChange={() => toggleSelection(asset.coinName)}
                 />
               </td>
               <td className="coin-info">
@@ -46,19 +52,28 @@ const Holdings = ({ CoinsList }) => {
                   <p>{asset.coin}</p>
                 </div>
               </td>
-                  <td>{asset.totalHolding}<br />{asset.averageBuyPrice}</td>
+              <td>
+                {asset.totalHolding}
+                <br />
+                {asset.averageBuyPrice}
+              </td>
               <td>{asset.currentPrice}</td>
               <td className="negative">
                 {asset.stcg.balance + asset.stcg.gain}
               </td>
-                <td>$0.00<br/>
+              <td>
+                $0.00
+                <br />
                 {asset.coin}
-                </td>
+              </td>
               <td>-</td>
             </tr>
           ))}
         </tbody>
       </table>
+      <button className="view-more" onClick={changeView}>
+        {updatedViewlist.length > 4 ? "View Less" : "View More"}
+      </button>
     </div>
   );
 };
